7031d81ab0a00c3ba689fc9fc87a2b52577ab602
makadown <maseflores79@gmail.com>
2019-03-17 11:48:43 -0600

    Repasando ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './componentes/ng-style/ng-style.component';
 import { CssComponent } from './componentes/css/css.component';
+import { ClasesComponent } from './componentes/clases/clases.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStyleComponent,
-    CssComponent
+    CssComponent,
+    ClasesComponent
   ],
   imports: [
     BrowserModule
