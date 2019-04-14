ffe93032fc3221991dc06a5cb2462792df9ba51e
makadown <maseflores79@gmail.com>
2019-04-14 17:47:35 -0500

    Directivas personalizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NgStyleComponent } from './componentes/ng-style/ng-style.component';
 import { CssComponent } from './componentes/css/css.component';
 import { ClasesComponent } from './componentes/clases/clases.component';
+import { ResaltadoDirective } from './directivas/resaltado.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStyleComponent,
     CssComponent,
-    ClasesComponent
+    ClasesComponent,
+    ResaltadoDirective
   ],
   imports: [
     BrowserModule
