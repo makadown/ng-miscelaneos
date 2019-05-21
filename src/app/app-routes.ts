b40a6c6f28fa2cc3344a1966477a8afd3b5b6ce0
Makadown <maseflores79@gmail.com>
2019-05-20 19:58:42 -0500

    Repasando rutas hijas

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -3,10 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './componentes/home/home.component';
 import { UsuarioComponent } from './componentes/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './componentes/usuario/usuario-nuevo.component';
+import { UsuarioDetalleComponent } from './componentes/usuario/usuario-detalle.component';
+import { UsuarioEditarComponent } from './componentes/usuario/usuario-editar.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
+    { path: 'usuario/:id', component: UsuarioComponent,
+      children: [
+        { path: 'nuevo', component: UsuarioNuevoComponent },
+        { path: 'editar', component: UsuarioEditarComponent },
+        { path: 'detalle', component: UsuarioDetalleComponent },
+        { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
+      ] },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
